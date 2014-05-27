4abd8b02999d94ab2a18bcc125535991084a3878
archaeron <whatamaps@gmail.com>
2014-05-27 22:36:54 +0200

    functions to print a term

---
+++ FLambdaCalculus/Print.fs
@@ -0,0 +1,29 @@
+﻿
+module Lambda.Print
+
+open Lambda.Eval
+
+let rec printText term =
+    match term with
+    | Constant c ->
+        match c with
+        | Num n -> sprintf "%i" n
+        | Add -> "+"
+        | Succ -> "Succ"
+        | IsZero -> "0"
+    | Variable v -> v
+    | Application (t1, t2) -> sprintf "(%s %s)" (printText t1) (printText t2)
+    | Lambda (x, t) -> sprintf "λ%s . %s" x (printText t)
+
+
+let rec printLatex term =
+    match term with
+    | Constant c ->
+        match c with
+        | Num n -> sprintf "%i" n
+        | Add -> "+"
+        | Succ -> "Succ"
+        | IsZero -> "0"
+    | Variable v -> v
+    | Application (t1, t2) -> sprintf "(%s \; %s)" (printLatex t1) (printLatex t2)
+    | Lambda (x, t) -> sprintf "[\\lambda %s . %s]" x (printLatex t)
